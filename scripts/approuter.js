ea46cf272710f80a7b3dbfa5131244249fefc2e3
praWeb <prasanna.virigineni@gmail.com>
2016-03-10 00:12:56 +0000

    added route for documentation
    @approuter: added path to view generated document. Theres an issue in
    generating the document. So has to look into this, if required

--- scripts/approuter.js
+++ scripts/approuter.js
@@ -5,7 +5,10 @@ routes.config(['$routeProvider', function($routeProvider){
 $routeProvider.
 	/*have to configure controller loading to avoid multiple controll calls*/
 	when("/test",{
-		"templateUrl" : 'view/home.html',
+		"templateUrl" : 'view/min/home.ng.html',
+		"controller" : "AppController"
+	}).when("/docs",{
+		"templateUrl" : 'docs/index.html',
 		"controller" : "AppController"
 	}).otherwise({redirectTo:'/test'});
 
